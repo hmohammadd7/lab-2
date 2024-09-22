f5ac15feaa0ddb95da035bfe371e48dd3bf32e91
Hassnain Mohammad <hmohammad7@myseneca.ca>
2024-09-22 18:46:29 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+#Author: Hassnain Mohammad
+
+#Author ID: hmohammad7
+
+#Date Created: 2024/09/21
+
+
+import sys
+
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print (timer)
+    timer = timer - 1
+
+print ('blast off!')
+
