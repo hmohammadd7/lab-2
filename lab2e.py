6c03b8acd3a2ecabc2c7460cc8fa86f37ed4047f
Hassnain Mohammad <hmohammad7@myseneca.ca>
2024-09-22 18:46:15 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+#Author: Hassnain Mohammad
+
+#Author ID: hmohammad7
+
+#Date Created: 2024/09/21
+
+
+timer = int(10)
+
+while timer != 0:
+    print (timer)
+    timer = timer - 1
+
+print ('blast off!')
